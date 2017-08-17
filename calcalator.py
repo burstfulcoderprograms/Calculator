e21dc16387ec1f0b96b88b3c237e011dd6be2dab
root <bakariraheem@gmail.com>
2017-08-16 22:49:38 -0500

    version 1

---
+++ calcalator.py
@@ -0,0 +1,32 @@
+
+def subtract(x, y):
+    results = x - y
+    return results
+
+
+def add(t, y):
+    results = t + y
+    return results
+
+
+def multiply(k, y):
+    results = k * y
+    return results
+
+
+def divide(m, y):
+    results = m / y
+    return results
+
+
+subtract_results = subtract(10, 20)
+print(subtract_results)
+
+add_results = add(5, 10)
+print(add_results)
+
+multiply_results = multiply(9, 9)
+print(multiply_results)
+
+divide_results = divide(1302, 2)
+print(divide_results)
